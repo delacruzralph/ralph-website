36089594257d9bd56dfb91e4add4535e3f5a3204
delacruzralph <ralphlewisdelacruz@gmail.com>
2023-06-26 18:40:34 -0400

    navbar scrolling active + fun facts

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,4 +1,43 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
+const funFacts = [
+  'my favorite color is purple because of a dance team called Quest Crew?',
+  'I once won a powerlifting competition?',
+  'I can solve a Rubik\'s cube in under a minute?',
+  // Add as many fun facts as you like...
+];
+
 export default function Sidebar() {
+  const [activeSection, setActiveSection] = useState(null);
+  const [funFact, setFunFact] = useState('');
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      { threshold: 0.1 } // Adjust this value as needed
+    );
+
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * funFacts.length);
+    setFunFact(funFacts[randomIndex]);
+  }, []);
+
   return (
     <div style={{
       backgroundColor: 'darkgray',
@@ -20,22 +59,22 @@ export default function Sidebar() {
           margin: '0',
           padding: '0',
           textAlign: 'right',
-          fontSize: '1.75rem'
+          fontSize: 'clamp(1.5rem, 1.5vw, 5rem)'
         }}>
-          <li><a href="#about">About me</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#resume">Resume</a></li>
-          <li><a href="#contact">Contact me</a></li>
+          <li><a href="#about" style={{ color: activeSection === 'about' ? 'purple' : 'black' }}>About me</a></li>
+          <li><a href="#projects" style={{ color: activeSection === 'projects' ? 'purple' : 'black' }}>Projects</a></li>
+          <li><a href="#skills" style={{ color: activeSection === 'skills' ? 'purple' : 'black' }}>Skills</a></li>
+          <li><a href="#resume" style={{ color: activeSection === 'resume' ? 'purple' : 'black' }}>Resume</a></li>
+          <li><a href="#contact" style={{ color: activeSection === 'contact' ? 'purple' : 'black' }}>Contact me</a></li>
         </ul>
       </nav>
       <div style={{
-        fontSize: '0.75rem'
+        fontSize: 'clamp(1rem, 1vw, 3rem)'
       }} className="did-you-know">
         <h4>
           Did you know...
         </h4>
-        <p>my favorite color is purple because of a dance team called Quest Crew?</p>
+        <p>{funFact}</p>
       </div>
     </div>
   );
